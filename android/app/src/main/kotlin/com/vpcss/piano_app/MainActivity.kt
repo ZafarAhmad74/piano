01a434348a0844134fcae3b6db796c9e65515e1a
zafar <zafarahmad@yahoo.com>
2021-02-05 19:55:20 +0500

    Piano App For MITI Course

---
+++ android/app/src/main/kotlin/com/vpcss/piano_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vpcss.piano_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(PianoApp());
+
+class PianoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
